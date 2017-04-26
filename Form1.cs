dbdae3e16928375d27c76fcc55bb00fdc23584f8
AmilaZimic <amila_zimic@hotmail.com>
2017-04-26 10:12:45 +0200

    Calculating average

--- Form1.cs
+++ Form1.cs
@@ -19,8 +19,20 @@ namespace GitHubProjectTest
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string numbers = richTextBox1.Text;
+            string[] array = numbers.Split(',');
+            int[] inputs = Array.ConvertAll(array, int.Parse);
 
+            double average;
+            int sum = 0;
 
+            for (int i = 0; i <= (inputs.Length - 1); i++)
+            {
+                sum += inputs[i];
+            }
+
+            average = (double)sum / inputs.Length;
+            label1.Text = "The average of inputed numbers is: " + average.ToString("0.###");
         }
     }
 }
